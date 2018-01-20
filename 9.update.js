18f0cbdde6e326a6fa731280f7c9d68e975bd635
ghostchan <jinnchan@qq.com>
2018-01-20 20:02:42 +0800

    findAndModify

---
+++ 9.update.js
@@ -0,0 +1,13 @@
+// db.workmate.update({sex:1},{$set:{money:1000}},false,true)
+// var resultMessage=db.runCommand({getLastError:1})
+// printjson(resultMessage)
+
+var myModify={
+    findAndModify:"workmate",
+    query:{name:"JSPang"},
+    update:{$set:{age:18}},
+    new:true
+}
+
+var resultMessage=db.runCommand(myModify)
+printjson(resultMessage)
